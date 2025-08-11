399ab1f462e0695a9efb3451ca7f3e381341e0cb
Nicolas Fernandes <107145775+NickNinjaGit@users.noreply.github.com>
2025-08-11 13:54:36 -0300

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,6 +4,10 @@ import { UserService } from './user.service';
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService) {}
+    @Get()
+    helloWorld(): string {
+        return "ESTAMOS AO VIVO BRASIL!";
+    }
     @Post('signup')
     async signupUser(
         @Body() userData: Prisma.UserCreateInput
