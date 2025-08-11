ae3bf05ef144f458ae88688eb6fd3cdf8c80aa79
Nicolas Fernandes <107145775+NickNinjaGit@users.noreply.github.com>
2025-08-11 13:58:25 -0300

    fix: update user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
 import {Prisma, User as UserModel} from "@prisma/client";
 import { UserService } from './user.service';
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService) {}
+    
     @Get()
     helloWorld(): string {
         return "ESTAMOS AO VIVO BRASIL!";
     }
+    
     @Post('signup')
     async signupUser(
         @Body() userData: Prisma.UserCreateInput
