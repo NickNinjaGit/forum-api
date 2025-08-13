18cd6081da3b59b21746d1c87053d805c8f7740a
NickNinjaGit <balguing@gmail.com>
2025-08-13 11:21:15 -0300

    feat: user CRUD finished

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,40 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import {Prisma, User as UserModel} from "@prisma/client";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { Prisma, User as UserModel } from '@prisma/client';
 import { UserService } from './user.service';
 @Controller('user')
 export class UserController {
-    constructor(private userService: UserService) {}
-    @Post('signup')
-    async signupUser(
-        @Body() userData: Prisma.UserCreateInput
-    ) : Promise<UserModel> {
-        return this.userService.createUser(userData)
-    }
-
+  constructor(private userService: UserService) {}
+  @Post()
+  async signupUser(
+    @Body() userData: Prisma.UserCreateInput,
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<UserModel | null> {
+    return this.userService.user({ id: Number(id) });
+  }
+  @Patch(':id')
+  async updateUser(
+    @Body() userData: Prisma.UserUpdateInput,
+    @Param('id') id: string,
+  ): Promise<UserModel> {
+    return this.userService.updateUser({
+      where: { id: Number(id) },
+      data: userData,
+    });
+  }
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string) {
+    this.userService.deleteUser({ id: Number(id) });
+    return { message: 'User deleted successfully!' };
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,17 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
-
+import * as bcrypt from "bcrypt";
 @Injectable()
 export class UserService {
   @Inject()
   private readonly prisma: PrismaService;
 
   async createUser(data: Prisma.UserCreateInput): Promise<User> {
-    return this.prisma.user.create({
-      data,
-    });
+    const hashPassword = await bcrypt.hash(data.password, 10);
+
+
+    return this.prisma.user.create({data: {...data, password: hashPassword},});
   }
 
   async user(
